c83bbefc26bb162a2b025aa9b08c8aae28d0de2d
Seruf <alexche92@gmail.com>
2019-11-21 17:49:22 +0200

    npm audit fix

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -2,13 +2,14 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 
 const Menu = () => {
+
   return(
     <ul className={"menu"}>
       <li className={"menu__item"}>
-        <NavLink to="/" className={"menu__link"}>All</NavLink>
+        <NavLink to={"/"} className={"menu__link"} exact>All</NavLink>
       </li>
       <li className={"menu__item"}>
-        <NavLink to="/animals" className={"menu__link"}>Animals</NavLink>
+        <NavLink to={"/animals"} className={"menu__link"}>Animals</NavLink>
       </li>
       <li className={"menu__item"}>
         <NavLink to={"/food&drink"} className={"menu__link"}>Food&Drink</NavLink>
